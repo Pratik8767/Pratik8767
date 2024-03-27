ba97ccc347cdbf5f42b123e044ef50c0b958a1a3
jadhaopratik999@gmail.com <pratik.jadhav@datamatter.tech>
2024-03-27 18:17:19 +0530

    code updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi import FastAPI
+from result.api import ResultAPI
 from schedule.api import Schedule_Api
 from attendence.api import Attendance
 from user.user_api import UserApi
@@ -17,6 +17,7 @@ app.include_router(Schedule_Api.Router)
 app.include_router(EventAPI.router)
 app.include_router(LibraryAPI.router)
 app.include_router(BranchAPI.router)
+app.include_router(ResultAPI.router)
 
 
 
